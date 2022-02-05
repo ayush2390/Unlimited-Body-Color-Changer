23c7ad4320f459904e016feca3999f49dc1cbea4
ayush2390 <43995654+ayush2390@users.noreply.github.com>
2022-02-05 23:28:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector(".button--color").addEventListener("click", function () {
+  document.querySelector("body").style.backgroundColor = randomColor();
+});
